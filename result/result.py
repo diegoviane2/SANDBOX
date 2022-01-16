6f17a23fccd58782486dbc562220a0dbd20b0d3a
Diego Viane <digaodigao1@gmail.com>
2022-01-16 16:23:52 -0300

    Improving code on RESULT project

--- result/result.py
+++ result/result.py
@@ -1,34 +1,40 @@
-"""import csv
-
-with open('file.csv') as file:
-    reader = csv.DictReader(file)
-
-for row in reader:
-    print(row)"""
-
 import csv
-with open('result.csv', newline='') as x:
-    rslt = csv.reader(x)
-    for line in rslt:
-        result = line
+
+class Result():
+    # Compares results from QUINA lottery. 5 numbers game.
+    def __init__(self, result, dataset):
+     self.result =  result
+     self.dataset =  dataset
+
+    def calculate(self):    
+      with open(self.result, newline='') as x:
+        rslt = csv.reader(x)
+        for line in rslt:
+            result = line
 
 
-with open('dataset.csv', newline='') as f:
-    dataset = csv.reader(f)
-    l = 0
-    for row in dataset:
-         l =l + 1
-         for i in row:
-             if result[0] == i:
-                 print ("Match in " + str(row) + " value " + str(i) + " in line " + str(l))
-             elif result[1] == i:
-                 print ("Match in " + str(row) + " value " + str(i) + " in line " + str(l))
-             elif result[2] == i:
-                 print ("Match in " + str(row) + " value " + str(i) + " in line " + str(l))
-             elif result[3] == i:
-                 print ("Match in " + str(row) + " value " + str(i) + " in line " + str(l))
-             elif result[4] == i:
-                 print ("Match in " + str(row) + " value " + str(i) + " in line " + str(l))
+        with open(self.dataset, newline='') as f:
+            dataset = csv.reader(f)
+            l = 0
+            for row in dataset:
+                l =l + 1
+                for i in row:
+                    if result[0] == i:
+                        print ("Match in " + str(row) + " value " + str(i) + " in line " + str(l))
+                    elif result[1] == i:
+                        print ("Match in " + str(row) + " value " + str(i) + " in line " + str(l))
+                    elif result[2] == i:
+                        print ("Match in " + str(row) + " value " + str(i) + " in line " + str(l))
+                    elif result[3] == i:
+                        print ("Match in " + str(row) + " value " + str(i) + " in line " + str(l))
+                    elif result[4] == i:
+                        print ("Match in " + str(row) + " value " + str(i) + " in line " + str(l))
              
              
-             
+
+
+def main():
+    a = Result('result.csv', 'dataset.csv')
+    a.calculate()
+
+main()
