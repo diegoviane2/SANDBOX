3920dd0cb0ecd99419b3bd938871d34be56577a8
Diego Viane <digaodigao1@gmail.com>
2022-01-22 18:16:13 -0300

    Update on teste.sh

--- teste.sh
+++ teste.sh
@@ -1,24 +1,29 @@
-pod=5
+pod=6
 declare -a res=()
 
-#echo "jmeter-test-1-0   1/1     CrashLoopBackOff   4          2m38sbad-frontend-4s58z   0/1     CrashLoopBackOff   4          2m38s" | awk '{print $2}' | cut -d "/" -f 1
-#echo "jmeter-test-1-0   1/1     CrashLoopBackOff   4          2m38sbad-frontend-4s58z   0/1     CrashLoopBackOff   4          2m38s" | awk '{print $2}' | cut -d "/" -f 1
-#echo "jmeter-test-1-0   1/1     CrashLoopBackOff   4          2m38sbad-frontend-4s58z   0/1     CrashLoopBackOff   4          2m38s" | awk '{print $2}' | cut -d "/" -f 1
-#echo "jmeter-test-1-0   1/1     CrashLoopBackOff   4          2m38sbad-frontend-4s58z   0/1     CrashLoopBackOff   4          2m38s" | awk '{print $2}' | cut -d "/" -f 1
 it=$(seq $pod)
 
+
+#Scan SatefulSet status and store running pods values
 for i in $it;
 do
 res+=($(echo "jmeter-test-1-0   1/1     CrashLoopBackOff   4          2m38sbad-frontend-4s58z   0/1     CrashLoopBackOff   4          2m38s" | awk '{print $2}' | cut -d "/" -f 1))
-
-
 done
 
-echo ${res[@]}
 
 for a in $(seq ${#res[@]});
 do
-total_pods=`echo $total_pods + ${res[a]} | bc`
+sum=$(IFS=+; echo "$((${res[*]}))")
 done
 
-echo $total_pods
+
+echo $sum
+des=10
+
+
+if [ $sum -le $des ]; then
+    echo "Parametro 1 ($sum) é Menor a 2 ($des)."
+else
+    echo "Parametro 1 ($sum) é Maior a 2 ($des)."
+fi
+
