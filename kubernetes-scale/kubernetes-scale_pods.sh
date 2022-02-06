35644fca6fcc31981f1ca0d79598a4bbbd2afa49
Diego Viane <digaodigao1@gmail.com>
2022-02-06 10:13:36 -0300

    Added script kubernetes-scale

---
+++ kubernetes-scale/kubernetes-scale_pods.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# kubernetes-scale_pods.sh load-test sts jmeter-master 1
+
+namespace=$1
+kind=$2
+resource=$3
+replicas=$4
+
+
+echo "[ Initializing task - Scale pods ]"
+# Get pods status
+last_active=`kubectl -n $namespace get $kind $resource --no-headers | awk '{print $2}' | cut -d "/" -f 1 | tr -d '\n'`
+
+echo "[ Scaling $resource pods from $last_active to $replicas ... ]"
+
+result=`kubectl -n $namespace scale $kind $resource --replicas=$replicas`
+
+echo "[ $result ... `kubectl -n $namespace get $kind $resource --no-headers | awk '{print $2}' | cut -d "/" -f 1 | tr -d '\n'` pod(s) running ... ] [ OK ]"
+
+
+#echo "[ $resource has $running pod(s) running ... ]"
+echo "[ Task - Scale pods ... ] [DONE]"
