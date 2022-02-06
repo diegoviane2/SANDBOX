d5b3de30e5fdd854a3089f74a7c8f957bb7faa32
Diego Viane <digaodigao1@gmail.com>
2022-02-06 11:26:17 -0300

    Improvements on output texts

--- kubernetes-scale/kubernetes-scale_pods.sh
+++ kubernetes-scale/kubernetes-scale_pods.sh
@@ -7,17 +7,16 @@ kind=$2
 resource=$3
 replicas=$4
 
-
-echo "[ Initializing task - Scale pods ]"
+#echo "[ TASK: SCALE PODS ]"
+echo "  [ Initializing task - Scale pods ]"
 # Get pods status
 last_active=`kubectl -n $namespace get $kind $resource --no-headers | awk '{print $2}' | cut -d "/" -f 1 | tr -d '\n'`
 
-echo "[ Scaling $resource pods from $last_active to $replicas ... ]"
+echo "  [ Scaling $resource pods from $last_active to $replicas ... ]"
 
 result=`kubectl -n $namespace scale $kind $resource --replicas=$replicas`
 
-echo "[ $result ... `kubectl -n $namespace get $kind $resource --no-headers | awk '{print $2}' | cut -d "/" -f 1 | tr -d '\n'` pod(s) running ... ] [ OK ]"
+echo "  [ $result ... `kubectl -n $namespace get $kind $resource --no-headers | awk '{print $2}' | cut -d "/" -f 1 | tr -d '\n'` pod(s) running ... ] [ OK ]"
 
-
-#echo "[ $resource has $running pod(s) running ... ]"
-echo "[ Task - Scale pods ... ] [DONE]"
+echo "  [ Scale pods ... ] [DONE]"
+#echo "[ TASK: SCALE PODS ] [ COMPLETED! ]"
